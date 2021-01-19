a5573830f6dc44722896015db7fcacaa77ad0424
Nedim <kurbegovicnedim@gmail.com>
2021-01-19 18:35:13 +0100

    prettify code

--- src/database-connector.ts
+++ src/database-connector.ts
@@ -1,23 +1,25 @@
 import mongoose, { Mongoose } from "mongoose";
 
 export default class DatabaseConnector {
-    public connect(): Promise<Mongoose> {
-        return new Promise<Mongoose>((resolve, reject) => {
-            mongoose.set('useNewUrlParser', true);
-            mongoose.set('useFindAndModify', false);
-            mongoose.set('useCreateIndex', true);
-            mongoose
-                .connect(
-                    process.env.databaseUrl ? process.env.databaseUrl : "mongodb+srv://admin:Iq83ilABPTZW!hk@cluster0-rpim1.mongodb.net/reports?retryWrites=true&w=majority"
-                    )
-                .then((db) => {
-                   console.log("Connected to database!");
-                    resolve(db);
-                })
-                .catch((error) => {
-                  //  console.log("Connection failed!");
-                    reject(error);
-                });
+  public connect(): Promise<Mongoose> {
+    return new Promise<Mongoose>((resolve, reject) => {
+      mongoose.set("useNewUrlParser", true);
+      mongoose.set("useFindAndModify", false);
+      mongoose.set("useCreateIndex", true);
+      mongoose
+        .connect(
+          process.env.databaseUrl
+            ? process.env.databaseUrl
+            : "mongodb+srv://admin:Iq83ilABPTZW!hk@cluster0-rpim1.mongodb.net/reports?retryWrites=true&w=majority"
+        )
+        .then((db) => {
+          console.log("Connected to database!");
+          resolve(db);
+        })
+        .catch((error) => {
+          console.log("Connection failed!");
+          reject(error);
         });
-    }
+    });
+  }
 }
