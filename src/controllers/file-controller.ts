217d039d2a6634f668c775c9e7bd01f88358a618
Zejd Makic <zmakic@softraysolutions.com>
2021-07-10 12:51:39 +0200

    Removed redundant endpoints

--- src/controllers/file-controller.ts
+++ src/controllers/file-controller.ts
@@ -7,129 +7,6 @@ const Aws = require("aws-sdk");
 const express = require('express');
 const router = express.Router();
 
-router.get("/get-file", async (req, res) => {
-    // @ts-ignore
-    const cloudConvert = new CloudConvert(process.env.CLOUD_CONVERT_KEY);
-    try {
-        // @ts-ignore
-        let job = await cloudConvert.jobs.create({
-            "tasks": {
-                "import-1": {
-                    "operation": "import/s3",
-                    "bucket": "app.items.uploads",
-                    "region": "eu-central-1",
-                    "access_key_id": process.env.AWS_KEY,
-                    "secret_access_key": process.env.AWS_SECRET,
-                    "key": "my-file.docx"
-                },
-                "task-1": {
-                    "operation": "convert",
-                    "input_format": "docx",
-                    "output_format": "html",
-                    "engine": "office",
-                    "input": [
-                        "import-1"
-                    ]
-                },
-                "export-1": {
-                    "operation": "export/url",
-                    "input": [
-                        "task-1"
-                    ],
-                }
-            }
-        });
-        const https = require('https');
-        job = await cloudConvert.jobs.wait(job.id);
-        const exportTask: any = job.tasks.filter(
-            task => task.operation === 'export/url' && task.status === 'finished'
-        )[0];
-        const file = exportTask.result.files[0];
-
-        const writeStream = fs.createWriteStream(file.filename);
-
-        console.log(file.url);
-        https.get(file.url, function (response) {
-            response.pipe(writeStream);
-        });
-
-        await new Promise((resolve, reject) => {
-            writeStream.on('finish', () => { resolve(true) });
-            writeStream.on('error', reject);
-        });
-
-        const path = require("path");
-        const absolutePath = path.resolve('./my-file.html');
-        res.status(200).sendFile(absolutePath);
-    } catch (e) {
-        res.status(400).json({status: 400, message: "Converting docx to html failed! " + e})
-    }
-});
-
-router.post("/html-upload", async (req, res) => {
-    // @ts-ignore
-    const cloudConvert = new CloudConvert(process.env.CLOUD_CONVERT_KEY);
-
-    try {
-        // @ts-ignore
-        await cloudConvert.jobs.create({
-            "tasks": {
-                "import-1": {
-                    "operation": "import/raw",
-                    "file": req.body.file,
-                    "filename": "my-file.html"
-                },
-                "export-1": {
-                    "operation": "export/s3",
-                    "input": [
-                        "import-1"
-                    ],
-                    "bucket": "app.items.uploads",
-                    "region": "eu-central-1",
-                    "access_key_id": process.env.AWS_KEY,
-                    "secret_access_key": process.env.AWS_SECRET
-                }
-            }
-        });
-
-        await cloudConvert.jobs.create({
-            "tasks": {
-                "import-1": {
-                    "operation": "import/s3",
-                    "bucket": "app.items.uploads",
-                    "region": "eu-central-1",
-                    "access_key_id": process.env.AWS_KEY,
-                    "secret_access_key": process.env.AWS_SECRET,
-                    "key": "my-file.html"
-                },
-                "task-1": {
-                    "operation": "convert",
-                    "input_format": "html",
-                    "output_format": "docx",
-                    "engine": "office",
-                    "input": [
-                        "import-1"
-                    ],
-                    "embed_images": false
-                },
-                "export-1": {
-                    "operation": "export/s3",
-                    "input": [
-                        "task-1"
-                    ],
-                    "bucket": "app.items.uploads",
-                    "region": "eu-central-1",
-                    "access_key_id": process.env.AWS_KEY,
-                    "secret_access_key": process.env.AWS_SECRET
-                }
-            }
-        });
-        res.status(200).json({status: 200, message: "Converting html to docx succeeded!"})
-    } catch (e) {
-        res.status(400).json({status: 400, message: "Converting html to docx failed! " + e})
-    }
-});
-
 router.get("/get-file/:fileName", async (req, res) => {
     console.log(req.params.fileName)
     // @ts-ignore
@@ -160,7 +37,7 @@ router.get("/get-file/:fileName", async (req, res) => {
             task => task.operation === 'export/url' && task.status === 'finished'
         )[0];
         console.log(exportTask)
-     //   if (exportTask) {
+        if (exportTask) {
             const file = exportTask.result.files[0];
 
             const writeStream = fs.createWriteStream(file.filename);
@@ -178,9 +55,9 @@ router.get("/get-file/:fileName", async (req, res) => {
             const path = require("path");
             const absolutePath = path.resolve('./' + req.params.fileName + '.html');
             res.status(200).sendFile(absolutePath);
-      //  } else {
-      //      res.send(200, 'emptyFile')
-      //  }
+        } else {
+            res.send(200, 'emptyFile')
+        }
 
     } catch (e) {
         res.status(400).json({status: 400, message: "Converting docx to html failed! " + e})
